2228289c90c5eea1837d312068dc71ef2fe42015
Luan Jardim <luang.ajardim@gmail.com>
2024-03-06 22:31:58 -0300

    Wip: HomePage first apperance, only html

--- frontend/src/app/Shopping_cart/pages/HomePage/index.tsx
+++ frontend/src/app/Shopping_cart/pages/HomePage/index.tsx
@@ -3,37 +3,58 @@ import { useEffect, useState } from 'react';
 import { RestaurantItemsList } from '../../components/RestaurantList/index';
 import { UserContext } from '../../../../Provider';
 import { useContext } from 'react';
+import { Link } from 'react-router-dom';
 
 export const HomePage = () => {
     const api = new APIService();
     const [restaurants, setRestaurants] = useState([]);
-    const [isLoading, setIsLoading] = useState(true);
-    const { user, setUserContext } = useContext(UserContext);
+    const { user, setUserContext, setCartContext } = useContext(UserContext);
 
     useEffect(() => {
         const fetchRestaurants = async () => {
             setRestaurants((await api.getRestaurants()).data);
         };
-        fetchRestaurants().catch((error) => console.error(error));
-        setIsLoading(false);
-        setUserContext({ user: 'user' });
-    }, []);
+        const isUserLogged = async () => {
+            // TODO: CHECK FOR A POSSIBLE NOT EXISTING USER
+            // get the logged user while there is no authentication
+            const LoggedUser = await api.getUser(1);
+            setUserContext(LoggedUser);
+        };
+        const getCart = async () => {
+            if(user !== null) {
+                const sp = await api.getShoppingCart(user.id);
+                setCartContext(sp);
+            }
 
+        };
+        //run the functions in the correct order
+        fetchRestaurants().then(async () => {
+            await isUserLogged().then( async() => {
+                await getCart().then(() => {
+                });
+            });
+        });
+
+    }, []);
 
     return (
         <div>
             <h1>Restaurants</h1>
-            {isLoading || user === null ? (
-                <div>Carregando...</div>
+            {user === null ? (
+                <div>Not logged</div>
             ) : (
                 <ul>
-                    {restaurants.map((restaurant) => (
+                    {
+                        restaurants.length > 0 ?
+                        restaurants.map((restaurant) => (
                         <li key={restaurant.id}>
                             <RestaurantItemsList restaurant={restaurant} />
                         </li>
-                    ))}
+                        )) : <h2>No restaurants found</h2>
+                    }
                 </ul>
             )}
+        <Link to="/shopping_cart">Shopping Cart</Link>
         </div>
     );
 };
