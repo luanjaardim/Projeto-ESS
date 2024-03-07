14cfd6fe35b6b79a49d8debeab81aa9f9e28239a
Luan Jardim <luang.ajardim@gmail.com>
2024-03-06 22:40:37 -0300

    Wip: Restaurant Item call to ShoppingCart funcs

--- frontend/src/app/Shopping_cart/components/ItemOfRestaurant/index.tsx
+++ frontend/src/app/Shopping_cart/components/ItemOfRestaurant/index.tsx
@@ -1,9 +1,17 @@
+import { UserContext } from '../../../../Provider';
+import { addToCart } from '../../pages/ShoppingCartPage/index';
+import { useContext } from 'react';
+
 export const ItemOfRestaurant = ({ item }) => {
+    const {user, cart, setCartContext} = useContext(UserContext);
+
     return (
         <div>
             <h4>{item.name}</h4>
             <p>{item.price} $</p>
-            <button onClick={() => console.log('add to cart')}>Add to cart</button>
+            <button onClick={() => addToCart(user, item, {cart, setCartContext}) }>
+                Add to cart
+            </button>
         </div>
     )
 };
