24997631aea175e2cd750830ddf528e760ebe677
Luan Jardim <luang.ajardim@gmail.com>
2024-03-06 22:31:19 -0300

    Feat: New requests for APIService

--- frontend/src/shared/components/APIService/index.tsx
+++ frontend/src/shared/components/APIService/index.tsx
@@ -13,10 +13,57 @@ export default class APIService {
     }
 
     getRestaurants() {
-        return this.api.get('/restaurants');
+        return this.api.get('/restaurants/');
+    }
+
+    getAllItems() {
+        return this.api.get('/items/');
     }
 
     getItems(restaurantId: number) {
-        return this.api.get(`/restaurants/${restaurantId}/items`);
+        return this.api.get(`/restaurants/${restaurantId}/items/`);
     }
+
+    async getUser(userId: number) {
+        const users = await this.api.get(`/clients/`);
+        return users.data.find((user: any) => user.id === userId);
+    }
+
+    async getShoppingCart(userId: number) {
+        const cart = (await this.api.get(`/${userId}/shopping_cart/`)).data;
+        const items = (await this.getAllItems()).data;
+        const itemsInCart = [];
+        for(var i = 0; i < cart.length; i++){
+            items.find((item: any) => {
+                if (item.id === cart[i].itemId) {
+                    itemsInCart.push({ id: item.id, name: item.name, price: item.price, quantity: cart[i].quantity });
+                }
+            });
+        }
+        return itemsInCart;
+    }
+
+    async addItemToCart(userId: number, itemId: number) {
+        return this.api.post(`/${userId}/shopping_cart/`, JSON.stringify({ itemId: itemId }))
+            .then((response) => response)
+            .catch((error) => error.response);
+    }
+
+    async removeItemFromCart(userId: number, itemId: number) {
+        return this.api.delete(`/${userId}/shopping_cart/`, { data: { itemId: itemId } })
+            .then((response) => response)
+            .catch((error) => error.response);
+    }
+
+    async updateItemFromCart(userId: number, itemId: number, quantity: number) {
+        return this.api.put(`/${userId}/shopping_cart/`, JSON.stringify({ itemId: itemId, quantity: quantity }))
+            .then((response) => response)
+            .catch((error) => error.response);
+    }
+
+    async finishOrder(userId: number, price: number) {
+        const orderId = (await this.api.get(`/${userId}/shopping_cart/orderId`)).data.orderId;
+        return this.api.put(`/orders/${orderId}/`, JSON.stringify({ price }));
+    }
+
 }
