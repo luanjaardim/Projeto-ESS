ac8a2b2a8b682789b776c63d0554bfcea226cbfa
Luan Jardim <luang.ajardim@gmail.com>
2024-03-06 22:33:24 -0300

    Feat: Adding ShoppingCart on UserContext

--- frontend/src/Provider.tsx
+++ frontend/src/Provider.tsx
@@ -1,17 +1,26 @@
 import { createContext, useState } from 'react';
 
 //create context
-export const UserContext = createContext({ user: null, setUserContext: (user: any) => {} });
+export const UserContext = createContext(
+    { user: null,
+      setUserContext: (user: any) => {},
+      cart: [],
+      setCartContext: (cart: any) => {} }
+);
 
 export const UserProvider = ({ children }) => {
     const [user, setUser] = useState(null);
+    const [cart, setCart] = useState([]);
 
     const setUserContext = (user: any) => {
         setUser(user);
     };
+    const setCartContext = (cart: any) => {
+        setCart(cart);
+    };
 
     return (
-        <UserContext.Provider value={{ user, setUserContext }}>
+        <UserContext.Provider value={{ user, setUserContext, cart, setCartContext }}>
             {children}
         </UserContext.Provider>
     );
