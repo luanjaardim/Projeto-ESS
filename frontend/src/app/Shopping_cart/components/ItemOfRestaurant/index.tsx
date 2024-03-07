0c9456708779844c04103be20644b8100be7b57c
Luan Jardim <luang.ajardim@gmail.com>
2024-03-07 08:44:37 -0300

    Wip: CSS for ItemOfRestaurant

--- frontend/src/app/Shopping_cart/components/ItemOfRestaurant/index.tsx
+++ frontend/src/app/Shopping_cart/components/ItemOfRestaurant/index.tsx
@@ -1,16 +1,19 @@
 import { UserContext } from '../../../../Provider';
 import { addToCart } from '../../pages/ShoppingCartPage/index';
 import { useContext } from 'react';
+import './styles.css';
 
 export const ItemOfRestaurant = ({ item }) => {
     const {user, cart, setCartContext} = useContext(UserContext);
 
     return (
-        <div>
-            <h4>{item.name}</h4>
-            <p>{item.price} $</p>
-            <button onClick={() => addToCart(user, item, {cart, setCartContext}) }>
-                Add to cart
+        <div className="item_of_restaurant">
+            <h4 className="item_of_restaurant_text">{item.name}</h4>
+            <p className="item_of_restaurant_text">{item.price} $</p>
+            <button
+                className="button_of_item_of_restaurant"
+                onClick={() => addToCart(user, item, {cart, setCartContext}) }>
+                    Add to cart
             </button>
         </div>
     )
