60e2cde0003a0831cad6054623f117f86f194332
HugoZig <ham4@cin.ufpe.br>
2024-03-08 16:39:17 -0300

    feat: insert order list query from frontend to backend

--- frontend/src/app/OrderCancellation/pages/index.tsx
+++ frontend/src/app/OrderCancellation/pages/index.tsx
@@ -1,54 +1,82 @@
-import {useState} from 'react';
+import {useState, useEffect, useContext} from 'react';
 import './style.css';
-
+import APIService from '../../../shared/components/APIService';
+import { UserContext } from '../../../Provider';
+import { Navigate, useNavigate } from 'react-router-dom';
+//import { OrderItemsList } from '../../components/RestaurantList/index';
 
 
 export const OrdersPage = () => {
-  // Estado para controlar a exibição da janela modal
-  const [modalOpen, setModalOpen] = useState(false);
+  const [modalOpen, setModalOpen] = useState(false)
+  const [orders, setOrders] = useState([]);;
+  const { user, setUserContext } = useContext(UserContext);
 
-  // Função para abrir a janela modal
   const openModal = () => {
     setModalOpen(true);
   };
 
-  // Função para fechar a janela modal
   const closeModal = () => {
     setModalOpen(false);
   };
 
+  const navigate = useNavigate();
+  const goBackToHome = () => {
+    navigate('/home');
+  };
+
+  const api = new APIService();
+
+  useEffect(() => {
+    // Função para obter os pedidos da API
+    const fetchOrders = async () => {
+      try {
+        const response = await api.getOrders(user?.id, user?.password);
+        setOrders(response.data); // Define os pedidos no estado
+      } catch (error) {
+        console.error('Erro ao obter os pedidos:', error);
+      }
+      setUserContext({ id: 10349, password: "senha_userId10349", nome: "Hugo" });
+    };
+
+    // Chama a função para obter os pedidos da API
+    fetchOrders();
+  }, []); // Executa somente uma vez após a montagem do componente
 
   return (
     <div className="orders-page">
       <header className="header">
-        <div className="back-button">Voltar</div>
+        <div className="back-button" onClick={goBackToHome}>Voltar</div>
         <div className="app-name">iBreno ;) </div>
         <div className="user-info">
-          <div className="user-name">João</div>
+          <div className="user-name">{user?.nome}</div>
           <img src="src/app/OrderCancellation/pages/mobolado.png" alt="Ícone do usuário" className="user-icon" />
           <button className="profile-button"></button>
         </div>
       </header>
       <main className="order-list">
         <div className="orders-title">Pedidos</div> {/* Caixa de indicação "Pedidos" */}
-        <div className="order">
-          <div className="order-label">Pedido #1</div> {/* Rótulo do pedido */}
-          <div className="order-details">
-            <div className="item-and-price">
-              <div className="item"><br />Item 1</div>
-              <div className="price">R$ 10,00</div>
+        {orders.map((order, index) => (
+          <div className="order" key={index}>
+            <div className="order-label">Pedido #{order.id}</div> {/* Rótulo do pedido */}
+            <div className="order-details">
+            {order.products.map((products, index) => (
+            <div className="item-and-price" key={index}>
+              <div className="item">{products.itemId}</div>
+              <div className="price">{products.quantity} X R$ 10.00</div>
             </div>
-            <div className="total">Total: R$ 10,00</div>
-          </div>
-          <div className="order-actions">
-            <div className="action-left">
-              <div className="status">Status: Em andamento</div>
-              <div className="time">Tempo estimado: 01:23</div>
+            ))}
+              <div className="total">Total: R${order.price.toFixed(2)}</div>
             </div>
-            <button className="cancel-button" onClick={openModal}> Cancelar <br /> Pedido
-            </button>
+            <div className="order-actions">
+              <div className="action-left">
+                <div className="status">Status: {order.status}</div>
+                <div className="time">Tempo estimado: {order.time}</div>
+              </div>
+              <button className="cancel-button" onClick={openModal}> Cancelar <br /> Pedido
+              </button>
+            </div>
           </div>
-        </div>
+        ))}
       </main>
       {modalOpen && (
         <div className="modal">
