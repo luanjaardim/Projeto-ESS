29c66f115cdb43dfefd1a2b4373be1d9a7707b5f
HugoZig <ham4@cin.ufpe.br>
2024-03-09 14:56:23 -0300

    fix: delete duplicated functions at APIService

--- frontend/src/shared/components/APIService/index.tsx
+++ frontend/src/shared/components/APIService/index.tsx
@@ -40,81 +40,73 @@ export default class APIService {
     return this.api.put(`/restaurants/${restaurantId}`, restaurant);
   }
 
-    getRestaurants() {
-        return this.api.get('/restaurants/');
-    }
+  getAllItems() {
+      return this.api.get('/items/');
+  }
 
-    getAllItems() {
-        return this.api.get('/items/');
-    }
+  async getUser(userId: number) {
+      const users = await this.api.get(`/clients/`);
+      return users.data.find((user: any) => user.id === userId);
+  }
 
-    getItems(restaurantId: number) {
-        return this.api.get(`/restaurants/${restaurantId}/items/`);
-    }
+  async getShoppingCart(userId: number) {
+      const cart = (await this.api.get(`/${userId}/shopping_cart/`)).data;
+      const items = (await this.getAllItems()).data;
+      const itemsInCart = [];
+      for(var i = 0; i < cart.length; i++){
+          items.find((item: any) => {
+              if (item.id === cart[i].itemId) {
+                  itemsInCart.push({ id: item.id, name: item.name, price: item.price, quantity: cart[i].quantity });
+              }
+          });
+      }
+      return itemsInCart;
+  }
 
-    async getUser(userId: number) {
-        const users = await this.api.get(`/clients/`);
-        return users.data.find((user: any) => user.id === userId);
-    }
+  async addItemToCart(userId: number, itemId: number) {
+      return this.api.post(`/${userId}/shopping_cart/`, JSON.stringify({ itemId: itemId }))
+          .then((response) => response)
+          .catch((error) => error.response);
+  }
 
-    async getShoppingCart(userId: number) {
-        const cart = (await this.api.get(`/${userId}/shopping_cart/`)).data;
-        const items = (await this.getAllItems()).data;
-        const itemsInCart = [];
-        for(var i = 0; i < cart.length; i++){
-            items.find((item: any) => {
-                if (item.id === cart[i].itemId) {
-                    itemsInCart.push({ id: item.id, name: item.name, price: item.price, quantity: cart[i].quantity });
-                }
-            });
-        }
-        return itemsInCart;
-    }
+  async removeItemFromCart(userId: number, itemId: number) {
+      return this.api.delete(`/${userId}/shopping_cart/`, { data: { itemId: itemId } })
+          .then((response) => response)
+          .catch((error) => error.response);
+  }
 
-    async addItemToCart(userId: number, itemId: number) {
-        return this.api.post(`/${userId}/shopping_cart/`, JSON.stringify({ itemId: itemId }))
-            .then((response) => response)
-            .catch((error) => error.response);
-    }
+  async updateItemFromCart(userId: number, itemId: number, quantity: number) {
+      return this.api.put(`/${userId}/shopping_cart/`, JSON.stringify({ itemId: itemId, quantity: quantity }))
+          .then((response) => response)
+          .catch((error) => error.response);
+  }
 
-    async removeItemFromCart(userId: number, itemId: number) {
-        return this.api.delete(`/${userId}/shopping_cart/`, { data: { itemId: itemId } })
-            .then((response) => response)
-            .catch((error) => error.response);
-    }
+  async finishOrder(userId: number, price: number) {
+      const orderId = (await this.api.get(`/${userId}/shopping_cart/orderId`)).data.orderId;
+      return this.api.put(`/orders/${orderId}/`, JSON.stringify({ price }));
+  }
 
-    async updateItemFromCart(userId: number, itemId: number, quantity: number) {
-        return this.api.put(`/${userId}/shopping_cart/`, JSON.stringify({ itemId: itemId, quantity: quantity }))
-            .then((response) => response)
-            .catch((error) => error.response);
-    }
+  async getOrders(clientId: number, password: String) {
+      return this.api.post(`/clients/${clientId}/orders`, {password: password},);
+  }
 
-    async finishOrder(userId: number, price: number) {
-        const orderId = (await this.api.get(`/${userId}/shopping_cart/orderId`)).data.orderId;
-        return this.api.put(`/orders/${orderId}/`, JSON.stringify({ price }));
-    }
+  async cancelOrder(clientId: number, orderId: number, password: String, reason: String) {
+      return this.api.put(`/clients/${clientId}/orders/${orderId}/cancellation`, {password: password, reason: reason}); 
+  }
 
-    async getOrders(clientId: number, password: String) {
-        return this.api.post(`/clients/${clientId}/orders`, {password: password},);
-    }
+  postLoginClient(email: String, password: String) {
+      return this.api.post('/clients/login', {email: email, password: password});
+  }
 
-    async cancelOrder(clientId: number, orderId: number, password: String, reason: String) {
-        return this.api.put(`/clients/${clientId}/orders/${orderId}/cancellation`, {password: password, reason: reason}); 
-    }
+  postTokenClient(token: String) {
+      return this.api.post('/clients/home', {header: 'Bearer ' + token});
+  }
 
-    postLoginClient(email: String, password: String) {
-        return this.api.post('/clients/login', {email: email, password: password});
-    }
+  postLoginRestaurant(email: String, password: String) {
+      return this.api.post('/restaurant/login', {email: email, password: password});
+  }
 
-    postTokenClient(token: String) {
-        return this.api.post('/clients/home', {header: 'Bearer ' + token});
-    }
-
-    postLoginRestaurant(email: String, password: String) {
-        return this.api.post('/restaurant/login', {email: email, password: password});
-    }
-
-    postTokenRestaurant(token: String) {
-        return this.api.post('/restaurant/home', {header: 'Bearer ' + token});
-    }
+  postTokenRestaurant(token: String) {
+      return this.api.post('/restaurant/home', {header: 'Bearer ' + token});
+  }
 }
