e4528bcb96bec4621f4c53a45bff5153d998efb5
HugoZig <ham4@cin.ufpe.br>
2024-03-09 12:24:25 -0300

    feat: full interface between frontend and backend - all functionalities are working

--- frontend/src/app/OrderCancellation/pages/index.tsx
+++ frontend/src/app/OrderCancellation/pages/index.tsx
@@ -15,6 +15,11 @@ export const OrdersPage = () => {
   const [password, setPassword] = useState(''); // Estado para armazenar a senha
   const [orderToCancel, setOrderToCancel] = useState(0);
   const [orderOpen, setOrderOpen] = useState(false);
+  const [ansStatus, setAnsStatus] = useState("normal"); 
+
+  const setStatus = (nStatus) => {
+    setAnsStatus(nStatus);
+  } 
 
   const setCancel = (val) => {
     setOrderToCancel(val);
@@ -68,66 +73,95 @@ export const OrdersPage = () => {
       }
       setUserContext({ id: 10349, password: "senha_userId10349", nome: "Hugo" });
     };
+
     fetchOrders();
-  }, [count]);
+
+
+    if (ansStatus !== "normal") {
+      const timer = setTimeout(() => {
+      setStatus("normal");
+      }, 3500); 
+      return () => clearTimeout(timer);
+    };
+
+  }, [count, ansStatus]);
 
   const handleConfirmCancelamento = async () => {
       try {
         var response = await api.cancelOrder(user?.id, orderToCancel, password, reason);
-        console.log(response);
-      } catch (error) {
-        console.error('Erro ao obter os pedidos:', error);
+        setStatus("funcionou");
+        //console.log(ansStatus);
+      }
+      
+       catch (error) {
+        var ans = error.response;
+        if (ans.data.message == "Pedido não cancelado: pedido já cancelado!") {
+          setStatus("ja cancelado");
+          //console.log("ja cancelado colocado");
+        } 
+        else if (ans.data.message == "Pedido não cancelado: pedido já foi aceito!") {
+          setStatus("ja aceito");
+          //console.log("ja aceito colocado");
+        }
+        else if (ans.data.message == "Pedido não cancelado: tempo limite excedido!") {
+          setStatus("tempo limite");
+          //console.log("tempo limite colocado");
+        } 
+        else if (ans.data.message == "Pedido não cancelado: senha incorreta!") {
+          setStatus("senha incorreta");
+          //console.log("senha incorreta colocada");
+        }
+        else console.error('Erro ao cancelar o pedido:', error);
       }
       closeModal();
       setCtr();
   };
-
   return (
-    <div className="orders-page">
-      <header className="header">
-        <div className="back-button" onClick={goBackToHome}>Voltar</div>
-        <div className="app-name">iBreno ;) </div>
-        <div className="user-info">
-          <div className="user-name">{user?.nome}</div>
-          <img src="src/app/OrderCancellation/pages/mobolado.png" alt="Ícone do usuário" className="user-icon" />
-          <button className="profile-button"></button>
+    <div className="orders-page-cancPg">
+      <header className="header-cancPg">
+        <div className="back-button-cancPg" onClick={goBackToHome}>Voltar</div>
+        <div className="app-name-cancPg">iBreno ;) </div>
+        <div className="user-info-cancPg">
+          <div className="user-name-cancPg">{user?.nome}</div>
+          <img src="src/app/OrderCancellation/pages/mobolado.png" alt="Ícone do usuário" className="user-icon-cancPg" />
+          <button className="profile-button-cancPg"></button>
         </div>
       </header>
-      <main className="order-list">
-        <div className="orders-title">Pedidos {!orderOpen ? (<span className="arrow-icon" onClick={() => {setCtr(); openOrder();}}> 
-        <img src="src/app/OrderCancellation/pages/rArrow.png" alt="right arrow" className="arrow-icon" /></span>) : 
-        (<span className="arrow-icon" onClick={() => {setCtr(); closeOrder();}}>
-        <img src="src/app/OrderCancellation/pages/dArrow.png" alt="down arrow" className="arrow-icon" /></span>)}</div>
+      <main className="order-list-cancPg">
+        <div className="orders-title-cancPg">Pedidos {!orderOpen ? (<span className="arrow-icon-cancPg" onClick={() => {setCtr(); openOrder();}}> 
+        <img src="src/app/OrderCancellation/pages/rArrow.png" alt="right arrow" className="arrow-icon-cancPg" /></span>) : 
+        (<span className="arrow-icon-cancPg" onClick={() => {setCtr(); closeOrder();}}>
+        <img src="src/app/OrderCancellation/pages/dArrow.png" alt="down arrow" className="arrow-icon-cancPg" /></span>)}</div>
         {orderOpen && (
           <div>
             {orders.filter(order => order.status !== "Nao finalizado").map((order, index) => (
-              <div className="order" key={index}>
-                <div className="order-label">Pedido #{order.id}</div> {/* Rótulo do pedido */}
-                <div className="order-details">
+              <div className="order-cancPg" key={index}>
+                <div className="order-label-cancPg">Pedido #{order.id}</div> {/* Rótulo do pedido */}
+                <div className="order-details-cancPg">
                 {order.products.map((product, index) => {
                   const item = items.find((item) => item.id === product.itemId);
-                  return (<div className="item-and-price" key={index}>
-                    <div className="item">{item?.name}</div>
-                    <div className="price">{product.quantity} X R$ {item?.price.toFixed(2)}</div>
+                  return (<div className="item-and-price-cancPg" key={index}>
+                    <div className="item-cancPg">{item?.name}</div>
+                    <div className="price-cancPg">{product.quantity} X R$ {item?.price.toFixed(2)}</div>
                   </div>
                 )
                 })}
-                  <div className="total">Total: R${order.price.toFixed(2)}</div>
+                  <div className="total-cancPg">Total: R${order.price.toFixed(2)}</div>
                 </div>
-                <div className="order-actions">
-                  <div className="action-left">
-                    <div className="status">
+                <div className="order-actions-cancPg">
+                  <div className="action-left-cancPg">
+                    <div className="status-cancPg">
                       Status: {order.status === "Cancelado" ? (
-                        <span className="status-cancelado">Cancelado</span>
+                        <span className="status-cancelado-cancPg">Cancelado</span>
                       ) : order.status === "Aceito" ? (
-                        <span className="status-aceito">Aceito</span>
+                        <span className="status-aceito-cancPg">Aceito</span>
                       ) : (
-                        <span className="status-pendente">{order.status}</span>
+                        <span className="status-pendente-cancPg">{order.status}</span>
                       )}
                     </div>
-                    <div className="time">Tempo estimado: {order.time}</div>
+                    <div className="time-cancPg">Tempo estimado: {order.time}</div>
                   </div>
-                  <button className="cancel-button" onClick={() => {openModal(); setCancel(order.id);}}> Cancelar <br /> Pedido
+                  <button className="cancel-button-cancPg" onClick={() => {openModal(); setCancel(order.id);}}> Cancelar <br /> Pedido
                   </button>
                 </div>
               </div>
@@ -136,26 +170,38 @@ export const OrdersPage = () => {
         )}
       </main>
       {modalOpen && (
-        <div className="modal">
-          <div className="modal-content">
-            <span className="close-button" onClick={closeModal}>
+        <div className="modal-cancPg">
+          <div className="modal-content-cancPg">
+            <span className="close-button-cancPg" onClick={closeModal}>
               &times;
             </span>
-            <h2 className="modal-title">Você tem certeza que quer cancelar seu pedido?</h2>
-            <div className="modal-body">
-            <div className="input-group">
+            <h2 className="modal-title-cancPg">Você tem certeza que quer cancelar seu pedido?</h2>
+            <div className="modal-body-cancPg">
+            <div className="input-group-cancPg">
                 <label htmlFor="motivo">Motivo do cancelamento:</label>
-                <input type="text" id="motivo" value={reason} onChange={handleMotivoChange} placeholder="Digite o motivo aqui" />
+                <input type="text-reason" id="motivo" value={reason} onChange={handleMotivoChange} placeholder="Digite o motivo aqui" />
               </div>
-              <div className="input-group">
+              <div className="input-group-cancPg">
                 <label htmlFor="senha">Insira sua senha:</label>
                 <input type="password" id="senha" value={password} onChange={handleSenhaChange} placeholder="Digite sua senha aqui" />
               </div>
             </div>
-            <button className="confirm-button" onClick={handleConfirmCancelamento}>Confirmar Cancelamento</button>
+            <button className="confirm-button-cancPg" onClick={handleConfirmCancelamento}>Confirmar Cancelamento</button>
           </div>
         </div>
       )}
+      {ansStatus !== "normal" && (
+        <div className={ansStatus === "funcionou" ? "success-tab-cancPg" : "error-tab-cancPg"}>
+          <span className={ansStatus === "funcionou" ? "close-button-info-green-cancPg" : "close-button-info-cancPg"} onClick={() => setStatus("normal")}>&times;</span>
+          <p>{ansStatus === "ja cancelado" ? "Pedido já cancelado!" :
+          ansStatus === "ja aceito" ? "Pedido já foi aceito!" :
+          ansStatus === "tempo limite" ? "Tempo limite excedido!" :
+          ansStatus === "senha incorreta" ? "Senha incorreta!" :
+          ansStatus === "funcionou" ? "Cancelamento bem sucedido!" : ""}</p>
+        </div>
+)}
+
+
     </div>
   );
 };
