c6a0f0c15c30de6a4ab495b21bde91637f2b463b
caiofernandes <caio.fern58@gmail.com>
2024-03-10 00:14:42 -0300

    feat(login): login client page finished | added snackbar

--- frontend/src/app/Login/pages/LoginClient/index.tsx
+++ frontend/src/app/Login/pages/LoginClient/index.tsx
@@ -1,17 +1,30 @@
+import { Snackbar } from "@mui/material";
+import Slide from "@mui/material/Slide";
+import Alert from "@mui/material/Alert";
+
 import APIService from '../../../../shared/components/APIService/index';
 import { useEffect, useState } from 'react';
 import { UserContext } from '../../../../Provider';
 import { useContext } from 'react';
 import { Link , Navigate } from 'react-router-dom';
 import './styles.css';
+import IconButton from "../../../../shared/components/IconButton";
+import { FaCheck } from "react-icons/fa6";
 
 export const LoginClientPage = () => {
   const api = new APIService;
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [redirectToHome, setRedirectToHome] = useState(false);
+  const [isSnackbarOpen, setIsSnackbarOpen] = useState(false);
+  const [snackbarMessage, setSnackbarMessage] = useState<string>("Erro!");
 
-  const handleLogin = (req: Request, res: Response) => {
+  const handleSnackbarClose = () => {
+    setIsSnackbarOpen(false);
+  };
+
+  const handleLogin = async(e) => {
+    e.preventDefault();
   alert(`Tentativa de login do cliente com email: ${email} e senha: ${password}`);
 
   const userData = {
@@ -19,8 +32,16 @@ export const LoginClientPage = () => {
     password: password
   };
 
+      if(!(userData.email && userData.password)) {
+        alert('Erro ao fazer login2222');
+      setSnackbarMessage("Todos os campos devem ser preenchidos!");
+      setIsSnackbarOpen(true);
+      return;
+      }
+
   api.postLoginClient(userData.email, userData.password)
     .then(response => {
+
       const token = response.data.header;
       console.log('Token recebido:', token);
 
@@ -39,6 +60,10 @@ export const LoginClientPage = () => {
       console.error('Erro:', error);
       
       alert('Erro ao fazer login');
+      
+      setSnackbarMessage("Email e/ou senha incorretos");
+      setIsSnackbarOpen(true);
+      return;
     });
   };
 
@@ -77,13 +102,35 @@ export const LoginClientPage = () => {
           />
         </div>
         <div className="containerButtonForm">
-          <button type="button" onClick={handleLogin}>Login</button>
+        <IconButton
+            onClick={handleLogin}
+            icon={FaCheck}
+            color="#54b544"
+            text="Login"
+            type="submit"
+          />
           <Link to = '/recover/client'>
             <button className="recPasswordButton">Esqueci a Senha</button>
           </Link>
         </div>
       </form>
       </div>
+      <Snackbar
+          open={isSnackbarOpen}
+          autoHideDuration={6000}
+          onClose={handleSnackbarClose}
+          anchorOrigin={{ vertical: "bottom", horizontal: "center" }}
+          TransitionComponent={(props) => <Slide {...props} direction="up" />}
+        >
+          <Alert
+            onClose={handleSnackbarClose}
+            severity="error"
+            variant="filled"
+            sx={{ width: "100%" }}
+          >
+            {snackbarMessage}
+          </Alert>
+        </Snackbar>
     </div>
   );
 };
