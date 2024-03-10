78fcc067edc45aed5f5dfc9061f049f9de68b432
caiofernandes <caio.fern58@gmail.com>
2024-03-10 20:36:40 -0300

    refactor(login): deleted some unused functions, imports, lines(debug)and comments | fixed identation

--- frontend/src/app/Login/pages/LoginClient/index.tsx
+++ frontend/src/app/Login/pages/LoginClient/index.tsx
@@ -1,15 +1,14 @@
 import { Snackbar } from "@mui/material";
 import Slide from "@mui/material/Slide";
 import Alert from "@mui/material/Alert";
-
+import { Link , Navigate } from 'react-router-dom';
 import APIService from '../../../../shared/components/APIService/index';
 import { useEffect, useState } from 'react';
 import { UserContext } from '../../../../Provider';
 import { useContext } from 'react';
-import { Link , Navigate } from 'react-router-dom';
-import './styles.css';
-import IconButton from "../../../../shared/components/IconButton";
 import { FaCheck } from "react-icons/fa6";
+import IconButton from "../../../../shared/components/IconButton";
+import './styles.css';
 
 export const LoginClientPage = () => {
   const api = new APIService();
@@ -26,51 +25,44 @@ export const LoginClientPage = () => {
 
   const handleLogin = async(e) => {
     e.preventDefault();
-  //alert(`Tentativa de login do cliente com email: ${email} e senha: ${password}`);
 
-  const userData = {
-    email: email,
-    password: password
-  };
+    const userData = {
+      email: email,
+      password: password
+    };
 
-  if(!(userData.email && userData.password)) {
-    //alert('Erro ao fazer login2222');
-  setSnackbarMessage("Todos os campos devem ser preenchidos!");
-  setIsSnackbarOpen(true);
-  return;
-  }
+    if(!(userData.email && userData.password)) {
+    setSnackbarMessage("Todos os campos devem ser preenchidos!");
+    setIsSnackbarOpen(true);
+    return;
+    }
 
-  api.postLoginClient(userData.email, userData.password)
-    .then(response => {
+    api
+      .postLoginClient(userData.email, userData.password)
+      .then(response => {
+        const token = response.data.header;
+        const client = response.data.client;
 
-      const token = response.data.header;
-      const client = response.data.client;
-      console.log('Client recebido:', token);
-      setUserContext({
-        id: client.id,
-        name: client.name,
-        email: client.email,
-        password: userData.password,
-        address: client.address,
-        cpf: client.cpf,
-      });
-
-      api.postTokenClient(token)
-        .then(tokenResponse => {
-          console.log('Dados do cliente:', tokenResponse.data);
-          
-          setRedirectToHome(true);
-        })
-        .catch(tokenError => {
-          console.log(token);
-          console.error('Erro ao obter token:', tokenError);
+        setUserContext({
+          id: client.id,
+          name: client.name,
+          email: client.email,
+          password: userData.password,
+          address: client.address,
+          cpf: client.cpf,
         });
+
+        api
+          .postTokenClient(token)
+          .then(tokenResponse => {
+            setRedirectToHome(true);
+          })
+          .catch(tokenError => {
+            console.error('Erro ao obter token:', tokenError);
+          });
     })
     .catch(error => {
       console.error('Erro:', error);
-      
-      //alert('Erro ao fazer login');
-      
       setSnackbarMessage("Email e/ou senha incorretos");
       setIsSnackbarOpen(true);
       return;
@@ -84,66 +76,67 @@ export const LoginClientPage = () => {
   return (
     <div style={{backgroundColor:"#fff13e"}}>
       <div style={{backgroundColor:"#eff1ed"}}>
-          <Link to = '/*'>
-            <button className="backButton">Voltar</button>
-          </Link>
-      <h1 className="titleLogin">Login Cliente</h1>
+        <Link to = '/*'>
+          <button className="backButton">Voltar</button>
+        </Link>
+        <h1 className="titleLogin">Login Cliente</h1>
       </div>
       <div className="containerForm">
-      <form className="loginForm">
-        <div>
-          <input
-            type="email"
-            id="email"
-            placeholder="E-mail"
-            value={email}
-            className="formFieldLogin"
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
-        </div>
-        <div>
-          <input
-            type="password"
-            id="senha"
-            placeholder="Senha"
-            value={password}
-            className="formFieldLogin"
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-        </div>
-        <div className="containerButtonForm">
-        <IconButton
-            onClick={handleLogin}
-            icon={FaCheck}
-            id="Login"
-            color="#54b544"
-            text="Login"
-            type="submit"
-          />
-          <Link to = '/clients/recover'>
-            <button className="recPasswordButton">Esqueci a Senha</button>
-          </Link>
-        </div>
-      </form>
+        <form className="loginForm">
+          <div>
+            <input
+              type="email"
+              id="email"
+              placeholder="E-mail"
+              value={email}
+              className="formFieldLogin"
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+          </div>
+          <div>
+            <input
+              type="password"
+              id="senha"
+              placeholder="Senha"
+              value={password}
+              className="formFieldLogin"
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+          </div>
+          <div className="containerButtonForm">
+            <IconButton
+              onClick={handleLogin}
+              icon={FaCheck}
+              id="Login"
+              color="#54b544"
+              text="Login"
+              type="submit"
+            />
+            <Link to = '/clients/recover'>
+              <button className="recPasswordButton">Esqueci a Senha</button>
+            </Link>
+          </div>
+        </form>
       </div>
+
       <Snackbar
-          open={isSnackbarOpen}
-          autoHideDuration={6000}
-          onClose={handleSnackbarClose}
-          anchorOrigin={{ vertical: "bottom", horizontal: "center" }}
-          TransitionComponent={(props) => <Slide {...props} direction="up" />}
+        open={isSnackbarOpen}
+        autoHideDuration={6000}
+        onClose={handleSnackbarClose}
+        anchorOrigin={{ vertical: "bottom", horizontal: "center" }}
+        TransitionComponent={(props) => <Slide {...props} direction="up" />}
         >
-          <Alert
-            onClose={handleSnackbarClose}
-            severity="error"
-            variant="filled"
-            sx={{ width: "100%" }}
-          >
-            {snackbarMessage}
-          </Alert>
-        </Snackbar>
+        <Alert
+          onClose={handleSnackbarClose}
+          severity="error"
+          variant="filled"
+          sx={{ width: "100%" }}
+        >
+          {snackbarMessage}
+        </Alert>
+      </Snackbar>
     </div>
   );
 };
