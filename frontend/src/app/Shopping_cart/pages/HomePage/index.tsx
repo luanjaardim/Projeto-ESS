9b8b1acea2250927128cbccfeb3f5ae20a805abc
Luan Jardim <luang.ajardim@gmail.com>
2024-03-09 23:29:54 -0300

    Fix: Navigation to Order erases context, fixed.

--- frontend/src/app/Shopping_cart/pages/HomePage/index.tsx
+++ frontend/src/app/Shopping_cart/pages/HomePage/index.tsx
@@ -4,6 +4,7 @@ import { RestaurantItemsList } from '../../components/RestaurantList/index';
 import { UserContext } from '../../../../Provider';
 import { useContext } from 'react';
 import { ShoppingCart } from '../../components/ShoppingCart/index';
+import { useNavigate } from 'react-router-dom';
 import './styles.css';
 
 export const transformIntoId = (name: string, with_hash: boolean) => {
@@ -13,18 +14,12 @@ export const transformIntoId = (name: string, with_hash: boolean) => {
 export const HomePage = () => {
     const api = new APIService();
     const [restaurants, setRestaurants] = useState([]);
-    const { user, setUserContext, setCartContext } = useContext(UserContext);
+    const { user, setCartContext } = useContext(UserContext);
     const [showCart, setShowCart] = useState(false);
 
     useEffect(() => {
         const fetchRestaurants = async () => {
             setRestaurants((await api.getRestaurants()).data);
-        };
-        const isUserLogged = async () => {
-            // TODO: CHECK FOR A POSSIBLE NOT EXISTING USER
-            // get the logged user while there is no authentication
-            const LoggedUser = await api.getUser(1);
-            setUserContext(LoggedUser);
         };
         const getCart = async () => {
             if(user !== null) {
@@ -35,13 +30,16 @@ export const HomePage = () => {
         };
         //run the functions in the correct order
         fetchRestaurants().then(async () => {
-            await isUserLogged().then( async() => {
-                await getCart().then(() => {
-                });
+            await getCart().then(() => {
             });
         });
 
     }, []);
+
+    const navigate = useNavigate();
+    const goToOrder = () => {
+        navigate('/order');
+    }
 
     return (
         <div style={{background: '#fffaba'}}>
@@ -51,6 +49,7 @@ export const HomePage = () => {
                     <button className="top_button">
                             <a href='../order'>
                                 Pedidos Finalizados </a>
+                            onClick={goToOrder}> Finished Orders
                     </button>
                     <button className="top_button" id="shopping_cart_button"
                             onClick={() => {setShowCart(!showCart)}}>
